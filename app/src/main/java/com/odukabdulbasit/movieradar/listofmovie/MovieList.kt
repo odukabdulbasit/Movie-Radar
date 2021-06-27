6b39d5cfdc8b1418ca3d4d28d3be3532cf87a7f9
Abdulbasit <2016212056@cumhuriyet.edu.tr>
2021-06-27 23:46:32 +0300

    deleted film finder fragment.kt

--- app/src/main/java/com/odukabdulbasit/movieradar/listofmovie/MovieList.kt
+++ app/src/main/java/com/odukabdulbasit/movieradar/listofmovie/MovieList.kt
@@ -39,15 +39,4 @@ class MovieList : Fragment() {
         return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId){
-        R.id.shake_to_find ->{
-            //shake ekranina yonlendirilecek
-            true
-        }
-        else -> super.onOptionsItemSelected(item)
-    }
 }
